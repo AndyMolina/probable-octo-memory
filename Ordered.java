adaa5a2ffaf76c23736c769e7d2d17257ab1decd
AndyMolina <87915253+AndyMolina@users.noreply.github.com>
2021-08-14 14:09:21 -0400

    Update Ordered.java

--- Ordered.java
+++ Ordered.java
@@ -4,9 +4,11 @@ public class Ordered
 {
    public static void main(String [] args)
    {
+      //Initializing Variables
       Scanner input = new Scanner(System.in);
       boolean isOrdered = false;
       
+      //Recieving Input for 3 Integers
       System.out.println("Enter Integer x: ");
       int x = input.nextInt();
       System.out.println("Enter Integer y: ");
@@ -14,7 +16,11 @@ public class Ordered
       System.out.println("Enter Integer z: "); 
       int z = input.nextInt();
    
+      //Prints out the 3 inputed integers
       System.out.println(x+" "+y+" "+z);
+      
+      //Compare the integers to each other and check if they are ordered in any way
+      //Then print true or false
       if((x>y && y>z) || (x<y && y<z)){
          isOrdered = true;
          System.out.println(isOrdered);
