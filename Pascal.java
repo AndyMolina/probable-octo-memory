ef1f9f74a87202d58673b99aeb831c9bc6464af0
AndyMolina <87915253+AndyMolina@users.noreply.github.com>
2021-08-14 14:23:53 -0400

    Update Pascal.java

--- Pascal.java
+++ Pascal.java
@@ -2,6 +2,7 @@
 import java.util.*;
 public class Pascal 
 {
+   //Takes an input of rows, uses that input and sends it to the pascal method, prints out the according triangle size
    public static void print(int n) 
    {
       for (int i = 0; i < n; i++) 
@@ -17,7 +18,8 @@ public class Pascal
          System.out.println();
       }
    }
-
+    
+   //Creates the pascal 
    public static int pascal(int i, int j)
    {
       if (j == 0 || j == i) 
@@ -25,15 +27,20 @@ public class Pascal
          return 1;
       } else 
       {
-         return pascal(i - 1, j - 1) + pascal(i - 1, j);
+         return pascal(i - 1, j - 1) + pascal(i - 1, j); //calls on itself
       }
    }
    
    public static void main(String[] args) 
    {
+      //Initialize Scanner object
       Scanner scanner = new Scanner(System.in);
+      
+      //Ask user how many rows
       System.out.print("Enter the number of rows to print: ");
       int rows = scanner.nextInt();
+      
+      //Print out triangle to user 
       System.out.println("Pascal Triangle:");
       print(rows);
       scanner.close();
